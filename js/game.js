422325a22367d99bc0c2cfb046f0f16e7e88fc41
〽️david <146224919+davidlreg@users.noreply.github.com>
2025-03-03 17:37:53 +0100

    first commit

---
+++ js/game.js
@@ -0,0 +1,11 @@
+let canvas;
+let ctx;
+let character = new Image();
+
+function init() {
+  canvas = document.getElementById("canvas");
+  ctx = canvas.getContext("2d");
+  character.src = "../asssets/img/2_character_pepe/1_idle/idle/I-1.png";
+
+  ctx.drawImage(character, 20, 20, 50, 100);
+}
