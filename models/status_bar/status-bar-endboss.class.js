b879f2141e13bc52fe47e5a172835e2742c63fe1
David Gerliz <davidgerliz@MacBook-Air-von-David.local>
2025-07-17 14:34:09 +0200

    added proper healtbar for the endboss

--- models/status_bar/status-bar-endboss.class.js
+++ models/status_bar/status-bar-endboss.class.js
@@ -3,10 +3,32 @@ class StatusBarEndboss extends StatusBar {
     "assets/img/7_statusbars/3_icons/icon_health_endboss.png",
   ];
 
+  IMAGES_STATUSBAR_ENDBOSS_HEALTH = [
+    "assets/img/7_statusbars/1_statusbar/2_statusbar_health/orange/0.png",
+    "assets/img/7_statusbars/1_statusbar/2_statusbar_health/orange/20.png",
+    "assets/img/7_statusbars/1_statusbar/2_statusbar_health/orange/40.png",
+    "assets/img/7_statusbars/1_statusbar/2_statusbar_health/orange/60.png",
+    "assets/img/7_statusbars/1_statusbar/2_statusbar_health/orange/80.png",
+    "assets/img/7_statusbars/1_statusbar/2_statusbar_health/orange/100.png",
+  ];
+
   constructor() {
     super().loadImage(this.IMAGE_ENDBOSS_HEALTH);
-    this.x = 620;
+    this.loadImages(this.IMAGES_STATUSBAR_ENDBOSS_HEALTH);
+    this.x = 500;
     this.y = 15;
+    this.width = 200;
+    this.height = 60;
+    this.endbossHealth = 5;
+  }
+
+  /**
+   * Checks if the icon image is fully loaded and ready to draw
+   *
+   * @returns {boolean} True if icon is loaded and ready
+   */
+  isIconLoaded() {
+    return this.img && this.img.complete && this.img.naturalWidth > 0;
   }
 
   /**
@@ -15,8 +37,36 @@ class StatusBarEndboss extends StatusBar {
    * @param {CanvasRenderingContext2D} ctx - The canvas rendering context.
    */
   draw(ctx) {
-    super.draw(ctx);
-    this.drawValue(ctx, this.endbossHealth);
+    ctx.save();
+    const centerX = this.x + this.width / 2;
+    ctx.translate(centerX, 0);
+    ctx.scale(-1, 1);
+    ctx.translate(-centerX, 0);
+    const healthBarImage = this.getCurrentHealthImage();
+    if (healthBarImage) {
+      ctx.drawImage(healthBarImage, this.x, this.y, this.width, this.height);
+    }
+    if (this.isIconLoaded()) {
+      const iconX = this.x + -20;
+      const iconY = this.y + 5;
+      const iconSize = 80;
+      ctx.drawImage(this.img, iconX, iconY, iconSize, iconSize);
+    }
+    ctx.restore();
+  }
+
+  /**
+   * Gets the current health bar image based on health level
+   *
+   * @returns {HTMLImageElement|null} Health bar image or null if not found
+   */
+  getCurrentHealthImage() {
+    if (this.endbossHealth === undefined || this.endbossHealth === null) {
+      this.endbossHealth = 5;
+    }
+    const healthIndex = Math.max(0, Math.min(5, this.endbossHealth));
+    const imagePath = this.IMAGES_STATUSBAR_ENDBOSS_HEALTH[healthIndex];
+    return this.imageCache[imagePath] || null;
   }
 
   /**
--- models/status_bar/status-bar-health.class.js
+++ models/status_bar/status-bar-health.class.js
@@ -1,11 +1,11 @@
 class StatusBarHealth extends StatusBar {
   IMAGES_STATUSBAR_HEALTH = [
-    "assets/img/7_statusbars/1_statusbar/2_statusbar_health/orange/0.png",
-    "assets/img/7_statusbars/1_statusbar/2_statusbar_health/orange/20.png",
-    "assets/img/7_statusbars/1_statusbar/2_statusbar_health/orange/40.png",
-    "assets/img/7_statusbars/1_statusbar/2_statusbar_health/orange/60.png",
-    "assets/img/7_statusbars/1_statusbar/2_statusbar_health/orange/80.png",
-    "assets/img/7_statusbars/1_statusbar/2_statusbar_health/orange/100.png",
+    "assets/img/7_statusbars/1_statusbar/2_statusbar_health/blue/0.png",
+    "assets/img/7_statusbars/1_statusbar/2_statusbar_health/blue/20.png",
+    "assets/img/7_statusbars/1_statusbar/2_statusbar_health/blue/40.png",
+    "assets/img/7_statusbars/1_statusbar/2_statusbar_health/blue/60.png",
+    "assets/img/7_statusbars/1_statusbar/2_statusbar_health/blue/80.png",
+    "assets/img/7_statusbars/1_statusbar/2_statusbar_health/blue/100.png",
   ];
 
   constructor() {
